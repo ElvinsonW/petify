0f0d5404a90410be5282a4610f15f2361358edb8
chelsealwna <chelsea.leawana@binus.ac.id>
2025-01-31 14:05:40 +0700

    commit

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,11 +7,6 @@ class EventController extends Controller
     // Menampilkan halaman index event
     public function index()
     {
-        // Logika untuk mengambil data event jika ada
-        // Misalnya jika ingin menampilkan data dari database
-        // $events = Event::all();  // Ambil semua data event dari database
-        
-        // Jika tidak ada data dari database, bisa kirim data statis atau kosong
         return view('event');  // Menampilkan view event.blade.php
     }
 }
