a29fa5e0f536d20e360131c3b13d45f6ee707460
chelsealwna <chelsea.leawana@binus.ac.id>
2025-02-04 09:29:30 +0700

    commit

--- resources/views/event/event.blade.php
+++ resources/views/event/event.blade.php
@@ -1,8 +1,7 @@
 <x-layout>
     
     <!-- container -->
-    <img src="{{ asset('images/homepage-dog.svg') }}">
-    <div class="container mx-auto py-8 my-8 px-4 overflow-hidden bg-no-repeat bg-center bg-contain" style="background-image: url(../src/images/adopt-bg.png)">
+    <div class="container mx-auto py-8 my-8 px-4 overflow-hidden bg-no-repeat bg-center bg-contain" style="background-image: url('{{ asset('images/adopt-bg.png') }}')">
         <!-- Main Content -->
         <div class="flex flex-col lg:flex-row lg:space-x-12">
             <!-- Left Section (Judul, Search, Filter, and Event List) -->
