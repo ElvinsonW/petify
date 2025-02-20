b54da3e96474ab973d618f78a1f57d5a42c52a99
Yehezkiel Michael <yehezkielmichaels@gmail.com>
2025-02-20 14:52:35 +0700

    fixing vw in indexadoptpost

--- resources/views/adoption/indexAdoptionPost.blade.php
+++ resources/views/adoption/indexAdoptionPost.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     @if (session('ownerError'))
         
-        <div id="alert-2" class="flex items-center p-4 mb-4 text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+        <div id="alert-2" class="flex items-center p-[1vw] mb-[1vw] text-red-800 rounded-[0.5vw] bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
             <svg class="shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
             </svg>
@@ -20,7 +20,7 @@
     @elseif (session('createSuccess'))
 
         <div id="alert" 
-            class="absolute z-40 flex items-center justify-center p-4 mb-4 w-[30vw] text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" 
+            class="absolute z-40 flex items-center justify-center p-4 mb-[1vw] w-[30vw] text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" 
             style="top: 10%; left: 50%; transform: translate(-50%, -50%);" 
             role="alert">
             <svg class="shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -50,7 +50,7 @@
             
             <!-- Search Bar -->
             <div>
-                <form class="max-w-md w-[16vw] mt-4" method="GET" action="{{ url('adoptions') }}">
+                <form class="max-w-md w-[16vw] mt-[1vw]" method="GET" action="{{ url('adoptions') }}">
                     @php
                         $params = ['category', 'liked'];
                     @endphp
@@ -61,13 +61,13 @@
                         @endif
                     @endforeach
             
-                    <label for="search" class="mb-2 text-sm text-gray-900 sr-only !font-overpass font-semibold">Search</label>
+                    <label for="search" class="mb-[0.5vw] text-sm text-gray-900 sr-only !font-overpass font-semibold">Search</label>
                     <div class="relative">
-                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-[0.75vw] pointer-events-none">
                             <i class="fa-solid fa-magnifying-glass"></i>
                         </div>
-                        <input type="search" id="search" name="search" class="block w-full p-4 ps-10 !font-overpass font-semibold border-1/2 border-gray-400 rounded-lg bg-white shadow-md" value="{{ request('search') }}" placeholder="Search Here...">
-                        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-greentipis hover:bg-greentua rounded-lg px-2 py-2 !font-overpass">Search</button>
+                        <input type="search" id="search" name="search" class="block w-full p-[1vw] ps-10 !font-overpass font-semibold border-1/2 border-gray-400 rounded-[0.5vw] bg-white shadow-md" value="{{ request('search') }}" placeholder="Search Here...">
+                        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-greentipis hover:bg-greentua rounded-lg px-[0.5vw] py-[0.5vw] !font-overpass">Search</button>
                     </div>
                 </form>
             </div>
@@ -78,20 +78,20 @@
                 $queryParams["like"] = 'true';
             @endphp
             <div class="flex flex-row">
-                <button class="mt-4 text-white bg-greenpetify rounded-2xl shadow-lg transform hover:scale-95 hover:bg-greentua transition duration-300 ease-in-out text-lg font-semibold px-2.5 py-2.5 font-overpass"><i class="fa-solid fa-sliders mr-2" style="color: #ffffff;"></i>Filter</button>
-                <button class="mt-4 ml-4 text-white bg-oren rounded-2xl shadow-lg transform hover:scale-95 hover:bg-orange-800 transition duration-300 ease-in-out text-lg font-semibold px-3 py-2.5 font-overpass"><i class="fa-solid fa-heart mr-2" style="color: #ffffff;"><a href="{{ url('adoptions') . '?' . http_build_query($queryParams) }}"></i>Your Like</a></button>
+                <button class="mt-[1vw] text-white bg-greenpetify rounded-[1vw] shadow-lg transform hover:scale-95 hover:bg-greentua transition duration-300 ease-in-out text-[1.2vw] font-semibold px-[0.75vw] py-[0.75vw] font-overpass"><i class="fa-solid fa-sliders mr-2" style="color: #ffffff;"></i>Filter</button>
+                <button class="mt-[1vw] ml-[1vw] text-white bg-oren rounded-[1vw] shadow-lg transform hover:scale-95 hover:bg-orange-800 transition duration-300 ease-in-out text-[1.2vw] font-semibold px-[0.75vw] py-[0.75vw] font-overpass"><i class="fa-solid fa-heart mr-2" style="color: #ffffff;"><a href="{{ url('adoptions') . '?' . http_build_query($queryParams) }}"></i>Your Like</a></button>
             </div>
             
             <!-- Button Adoption Post -->
             <a href="/adoptions/create">
-                <button class="mt-4 w-5/6 text-white bg-orenmuda rounded-2xl shadow-lg transform hover:scale-95 hover:bg-orange-400 transition duration-300 ease-in-out text-lg font-semibold px-3 py-2.5 font-overpass"><i class="fa-solid fa-plus mr-2" style="color: #ffffff;"></i>Adoption Post</button>
+                <button class="mt-[1vw] w-[16vw] text-white bg-orenmuda rounded-[1vw] shadow-lg transform hover:scale-95 hover:bg-orange-400 transition duration-300 ease-in-out text-[1.2vw] font-semibold px-[0.75vw] py-[0.75vw] font-overpass"><i class="fa-solid fa-plus mr-2" style="color: #ffffff;"></i>Adoption Post</button>
             </a>
             
             <!-- Container Pet Category -->
-            <div class="mt-4 w-5/6 shadow-2xl rounded-lg border-1/2 border-gray-400 p-4 font-montserrat_alt">
-                <div class="pl-2">
-                    <h4 class="text-xl font-bold">Pet Category</h4>
-                    <hr class="border-black border-1/2 w-full my-2">
+            <div class="mt-[1vw] w-[16vw] shadow-2xl rounded-[0.5vw] border-1/2 border-gray-400 p-[1vw] font-montserrat_alt">
+                <div class="pl-[0.5vw]">
+                    <h4 class="text-[1.4vw] font-bold">Pet Category</h4>
+                    <hr class="border-black border-1/2 w-full my-[0.5vw]">
                 </div>
                 
                 <!-- ALL CATEGORY -->
@@ -100,10 +100,10 @@
                         $queryParams = request()->query();
                     @endphp
 
-                    <button class="clear-category pl-2 pr-2 w-full transition duration-500 ease-in-out rounded-xl group bg-orenmuda">
+                    <button class="clear-category pl-[0.5vw] pr-[0.5vw] w-full transition duration-500 ease-in-out rounded-[0.75vw] group bg-orenmuda">
                         <a href="{{ url('adoptions/') . '?' . http_build_query($queryParams) }}">
-                            <p class="text-xl font-semibold mt-2 text-left text-white">All Category</p>
-                            <hr class="border-1/2 my-2 w-full border-white">
+                            <p class="text-[1.4vw] font-semibold mt-[0.5vw] text-left text-white">All Category</p>
+                            <hr class="border-1/2 my-[0.5vw] w-full border-white">
                         </a>
                     </button>         
 
@@ -112,10 +112,10 @@
                         $queryParams = request()->query();
                         unset($queryParams['category']); 
                     @endphp
-                    <button class="clear-category pl-2 pr-2 w-full transition duration-500 ease-in-out rounded-xl group hover:bg-orenmuda">
+                    <button class="clear-category pl-[0.5vw] pr-[0.5vw] w-full transition duration-500 ease-in-out rounded-[0.75vw] group hover:bg-orenmuda">
                         <a href="{{ url('adoptions/') . '?' . http_build_query($queryParams) }}">
-                            <p class="text-xl font-semibold mt-2 text-left group-hover:text-white transition-colors duration-500 ease-in-out">All Category</p>
-                            <hr class="border-orenmuda border-1/2 w-3/6 my-2 group-hover:w-full group-hover:border-white transition-all duration-500 ease-in-out">
+                            <p class="text-[1.4vw] font-semibold mt-[0.5vw] text-left group-hover:text-white transition-colors duration-500 ease-in-out">All Category</p>
+                            <hr class="border-orenmuda border-1/2 w-[6vw] my-[0.5vw] group-hover:w-full group-hover:border-white transition-all duration-500 ease-in-out">
                         </a>
                     </button>   
 
@@ -134,18 +134,18 @@
                         @php
                             unset($queryParams["category"]);
                         @endphp
-                        <button class="pl-2 pr-2 w-full rounded-xl group bg-{{ $category->color }}">
+                        <button class="pl-[0.5vw] pr-[0.5vw] w-full rounded-[0.75vw] group bg-{{ $category->color }}">
                             <a href="{{ url('/adoptions') . '?' . http_build_query($queryParams) }}" class="w-full">
-                                <p class="text-xl font-semibold mt-2 text-left text-white">{{ $category->name }}</p>
-                                <hr class="border-1/2 my-2 w-full border-white">
+                                <p class="text-[1.4vw] font-semibold mt-[0.5vw] text-left text-white">{{ $category->name }}</p>
+                                <hr class="border-1/2 my-[0.5vw] w-full border-white">
                             </a>
                         </button>
                 
                     @else
-                        <button class="pl-2 pr-2 w-full transition duration-500 ease-in-out rounded-xl group hover:bg-{{ $category->color }}">
+                        <button class="pl-[0.5vw] pr-[0.5vw] w-full transition duration-500 ease-in-out rounded-[0.75vw] group hover:bg-{{ $category->color }}">
                             <a href="{{ url('/adoptions') . '?' . http_build_query($queryParams) }}" class="w-full">
-                                <p class="text-xl font-semibold mt-2 text-left group-hover:text-white transition-colors duration-500 ease-in-out">{{ $category->name }}</p>
-                                <hr class="border-{{ $category->color }} border-1/2 w-3/6 my-2 group-hover:w-full group-hover:border-white transition-all duration-500 ease-in-out">
+                                <p class="text-[1.4vw] font-semibold mt-[0.5vw] text-left group-hover:text-white transition-colors duration-500 ease-in-out">{{ $category->name }}</p>
+                                <hr class="border-{{ $category->color }} border-1/2 w-[6vw] my-[0.5vw] group-hover:w-full group-hover:border-white transition-all duration-500 ease-in-out">
                             </a>
                         </button>
                     @endif
@@ -155,52 +155,52 @@
         <!-- Bagian Kiri (Sidebar) End -->
         
         <!-- Bagian Kanan (Konten) Start -->
-        <div class="ml-1 flex-1 pt-10">
+        <div class="ml-[0.25vw] flex-1 pt-[3vw]">
             <!-- Judul Page -->
             <div class="text-center">
-                <h2 class="text-6xl font-montserrat_alt font-bold text-greenpetify">Adopt your Pet!</h2>
-                <p class="text-xl font-open_sans font-semibold mt-4">Find the animal you want to care for</p>
+                <h2 class="text-[3.8vw] font-montserrat_alt font-bold text-greenpetify">Adopt your Pet!</h2>
+                <p class="text-[1.4vw] font-open_sans font-semibold mt-[1vw]">Find the animal you want to care for</p>
             </div>
 
             <!-- Pet Adopt Catalog -->
-            <div class="grid grid-cols-3 gap-15 mx-4 mt-4">
+            <div class="grid grid-cols-3 gap-[0.75vw] mx-[1vw] mt-[1vw]">
                 <!-- Catalog -->
                 @foreach ($adoptions as $adoption)
 
                     <a href="/adoptions/{{ $adoption->slug }}">
-                        <div class="rounded-lg shadow-xl p-2 h-[42vw] w-[24vw] mt-10">
+                        <div class="rounded-[0.5vw] shadow-xl p-[0.5vw] h-[42vw] w-[24vw] mt-[2.5vw]">
                             <!-- Gambar Pet -->
                             <img src="{{ asset('images/petadoptpic.svg') }}" alt="Pet Picture" class="w-full h-fit">
                             
                             <!-- Category & Days -->
                             <div class="flex flex-row font-montserrat_alt font-semibold w-full">
-                                <p class="w-fit rounded-xl bg-{{ $adoption->pet->pet_category->color }} text-xl text-center text-white my-4 py-1.5 px-2">{{ $adoption->pet->pet_category->name }}</p>     
-                                <p class="text-slate-400 my-4 ml-auto py-1.5 px-2">{{ $adoption->created_at->diffForHumans() }}</p>
+                                <p class="w-fit rounded-[0.75vw] bg-{{ $adoption->pet->pet_category->color }} text-[1.4vw] text-center text-white my-[1vw] py-[0.5vw] px-[0.5vw]">{{ $adoption->pet->pet_category->name }}</p>     
+                                <p class="text-slate-400 my-[1vw] ml-auto py-[0.5vw] px-[0.5vw]">{{ $adoption->created_at->diffForHumans() }}</p>
                             </div>
         
                             <!-- Name & Like -->
                             <div class="flex flex-row font-montserrat_alt font-semibold w-full min-h-[5vw]">
-                                <p class="text-2xl">{{ $adoption->name }}</p>    
+                                <p class="text-[1.6vw]">{{ $adoption->name }}</p>    
                                 <!-- Like -->
                                 @php
                                     $likedPostIds = $likedPosts->pluck('adoption_post_id')->toArray();
                                     $isLiked = in_array($adoption->id,$likedPostIds);
                                 @endphp
-                                <div class="mt-1 ml-auto pr-3">
+                                <div class="mt-[0.25vw] ml-auto pr-[0.75vw]">
                                     <i class="fa-solid fa-heart fa-lg likeIcon {{ $isLiked ? 'filled-heart' : '' }}" style="color: #a6a6a6; cursor: pointer;"></i>
                                 </div> 
                             </div>
         
                             <!-- Desc Singkat -->
-                            <p class="font-open_sans text-slate-600 mt-1 mb-6 text-justify pr-2 break-words min-h-[8vw]">{{ Str::limit($adoption->description,150) }}</p>
+                            <p class="font-open_sans text-slate-600 mt-[0.25vw] mb-[1.5vw] text-justify pr-[0.5vw] break-words min-h-[8vw]">{{ Str::limit($adoption->description,150) }}</p>
                             
                             <!-- Profile -->
                             <div class="flex flex-row">
-                                <div class="w-12 h-12 bg-white border-4 border-greentua rounded-full flex justify-center items-center">
+                                <div class="w-[3.2vw] h-[3.2vw] bg-white border-4 border-greentua rounded-full flex justify-center items-center">
                                     <img src="{{ asset('images/after login.svg') }}" alt="Profile Owner">
                                 </div>
-                                <p class="mx-2 mt-2 font-overpass font-semibold text-xl">{{ $adoption->user->username }}</p>
-                                <p class="mt-2 ml-auto px-2 font-montserrat_alt font-semibold"><i class="fa-solid fa-location-dot fa-xs" style="color: #cc4b4b;"></i> Tangerang</p>
+                                <p class="mx-[0.5vw] mt-[0.5vw] font-overpass font-semibold text-[1.4vw]">{{ $adoption->user->username }}</p>
+                                <p class="mt-[0.5vw] ml-auto px-[0.5vw] font-montserrat_alt font-semibold"><i class="fa-solid fa-location-dot fa-xs" style="color: #cc4b4b;"></i> Tangerang</p>
                             </div>
                         </div>
                     </a>
@@ -208,7 +208,7 @@
                 @endforeach
                 
             </div>
-            <div class="mx-12 my-10 text-xl">
+            <div class="mx-[3vw] my-[2.5vw] text-[1.4vw]">
                 {{ $adoptions->links() }}
             </div>
         </div>
