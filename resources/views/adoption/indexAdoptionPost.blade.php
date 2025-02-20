a9b80e52644ba5ae71619b10ee26746fcbfcbf0f
Yehezkiel Michael <yehezkielmichaels@gmail.com>
2025-02-20 22:55:18 +0700

    update search bar on indexadoptpost

--- resources/views/adoption/indexAdoptionPost.blade.php
+++ resources/views/adoption/indexAdoptionPost.blade.php
@@ -63,12 +63,12 @@
                     @endforeach
             
                     <label for="search" class="mb-[0.5vw] text-sm text-gray-900 sr-only !font-overpass font-semibold">Search</label>
-                    <div class="relative w-full border-1/2 border-gray-400 rounded-[0.1vw] bg-white shadow-md">
+                    <div class="relative w-full border-1/2 border-gray-400 rounded-[0.5vw] bg-white shadow-md">
                         <div class="absolute inset-y-0 start-0 flex items-center ps-[0.75vw] pointer-events-none">
                             <i class="fa-solid fa-magnifying-glass"></i>
                         </div>
-                        <input type="search" id="search" name="search" class="block w-full max-w-[calc(100%-5.2vw)] p-[1vw] ps-10 !font-overpass font-semibold" value="{{ request('search') }}" placeholder="Search Here">
-                        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-greentipis hover:bg-greentua rounded-[0.5vw] px-[0.5vw] py-[0.5vw] !font-overpass">Search</button>
+                        <input type="search" id="search" name="search" class="rounded-[0.5vw] block w-full max-w-[calc(100%-5.2vw)] p-[1vw] ps-10 !font-overpass font-semibold" value="{{ request('search') }}" placeholder="Search Here">
+                        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-greentipis hover:bg-greentua rounded-[0.5vw] px-[0.5vw] py-[0.4vw] !font-overpass">Search</button>
                     </div>
                 </form>
             </div>
